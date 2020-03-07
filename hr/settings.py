b2651d8704428dd464c881b13012ed1d13783d5a
Md Nazmul Hasan <farhapartex@gmail.com>
2020-03-07 17:46:44 +0600

    Pillow and psycopg2 installed

--- hr/settings.py
+++ hr/settings.py
@@ -81,11 +81,16 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hr_management",
+        "USER": "devadmin",
+        "HOST": "localhost",
+        "PASSWORD" : "Ninjacoder**08",
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
