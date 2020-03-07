1a54dd683ea080b355c2c5ebc19e78492631bdd0
Md Nazmul Hasan <farhapartex@gmail.com>
2020-03-07 21:21:02 +0600

    Company admin model added

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Designation)
+class Designation(admin.ModelAdmin):
+    list_display = ("name","salary","experience")
--- company/models.py
+++ company/models.py
@@ -9,7 +9,7 @@ def image_upload_path(instance, filename):
 class Company(Base):
     name = models.CharField(_("Company Name"), max_length=100)
     type = models.CharField(_("Company Type"), max_length=50)
-    description = models.TextField(_("Description"), black=True, null=True)
+    description = models.TextField(_("Description"), blank=True, null=True)
     height = models.IntegerField(_("Height"), blank=True, null=True)
     width = models.IntegerField(_("Width"), blank=True, null=True)
     logo = models.ImageField(
@@ -22,6 +22,9 @@ class Company(Base):
         null=True
     )
 
+    class Meta:
+        verbose_name_plural = 'Companies'
+
     def __str__(self):
         return self.name
 
@@ -30,6 +33,14 @@ class Branch(Base):
     company = models.ForeignKey(Company, verbose_name=_("Company"), on_delete=models.CASCADE)
     name = models.CharField(_("Name"), max_length=200)
     division = models.CharField(_("Division"), max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Designation(Base):
+    name = models.CharField(_("Name"), max_length=150)
+    experience = models.IntegerField(_("Experience"))
+    salary = models.DecimalField(_("Salary"), max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
